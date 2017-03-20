56a07f3b26965e2529bdbf4daa15b9c28ef8112c
Kathryn Clark <kccla@umich.edu>
2017-03-20 17:02:07 -0400

    adding main to test

--- test_rpn.py
+++ test_rpn.py
@@ -1,6 +1,7 @@
 import unittest
 import rpn
 
+
 class TestBasics(unittest.TestCase):
 	def test_add(self):
 		result = rpn.calculate('1 1 +')
@@ -8,3 +9,7 @@ class TestBasics(unittest.TestCase):
 	def test_subtract(self):
 		result = rpn.calculate('5 3 -')
 		self.assertEqual(2, result)
+        
+        
+if __name__ == '__main__':
+    unittest.main()
