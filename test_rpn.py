3d640003f430b7d5d55fbc6e36bc825b17e7078c
Kathryn Clark <kccla@umich.edu>
2017-03-20 17:19:58 -0400

    fixed?

--- test_rpn.py
+++ test_rpn.py
@@ -1,7 +1,6 @@
 import unittest
 import rpn
 
-
 class TestBasics(unittest.TestCase):
 	def test_add(self):
 		result = rpn.calculate('1 1 +')
@@ -9,7 +8,3 @@ class TestBasics(unittest.TestCase):
 	def test_subtract(self):
 		result = rpn.calculate('5 3 -')
 		self.assertEqual(2, result)
-        
-        
-if __name__ == '__main__':
-    unittest.main()
